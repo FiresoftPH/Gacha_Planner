221204857659291139bd833e76dcb5c62ea4af73
Phanuruj3146 <Phanuruj3146@gmail.com>
2023-10-09 19:46:53 +0700

    fixed topbar and gachaplanner page overlap

--- front-end/src/Components/TopBarComponent/Topbar.js
+++ front-end/src/Components/TopBarComponent/Topbar.js
@@ -6,28 +6,26 @@ import { Link, useLocation } from 'react-router-dom';
 
 export default function Topbar(){
 
-const navigateToGachaPlanner = () => {
-    navigate('/');
-    };
-const location = useLocation();
-const buttonYourplanner = location.pathname === '/yourplanner' ? 'topbar-button-clicked' : 'topbar-button';
-const buttonBannerHistory = location.pathname === '/bannerhistory' ? 'topbar-button-clicked' : 'topbar-button';
+    const location = useLocation();
+    const buttonGachaPlanner = location.pathname === '/' ? 'topbar-button-clicked' : 'topbar-button';
+    const buttonYourplanner = location.pathname === '/yourplanner' ? 'topbar-button-clicked' : 'topbar-button';
+    const buttonBannerHistory = location.pathname === '/bannerhistory' ? 'topbar-button-clicked' : 'topbar-button';
     
+const navigateToGachaPlanner = () => {
+        navigate('/');
+    };  
 
-
-const navigateToDestination = () => {
+const navigateToYourPlanner = () => {
     navigate('/yourplanner');
     };  
 
-const navigateToDestination2 = () => {
+const navigateToBannerHistory = () => {
     navigate('/bannerhistory');
     };  
-
 
 const navigateToLogin = () => {
     navigate('/login');
     };  
-
 
 const navigate = useNavigate();
 return(
@@ -35,12 +33,9 @@ return(
     <div className="topbar-header-pic-container">
         <img className='topbar-header-pic' src={myPic}></img>
     </div>
-    <button onClick={navigateToGachaPlanner} className='planner-button'> Gacha Planner</button>
-    <button onClick={navigateToDestination2} className='topbar-button'>Banner History</button>
-    <button onClick={navigateToDestination} className='topbar-button'>Your Planner</button>
-    <button className='topbar-button'>Gacha Planner</button>
-    <button onClick={navigateToDestination2}  className={buttonBannerHistory}>Banner History</button>
-    <button onClick={navigateToDestination} className={buttonYourplanner}>Your Planner</button>
+    <button onClick={navigateToGachaPlanner}  className={buttonGachaPlanner}>Gacha Planner</button>
+    <button onClick={navigateToBannerHistory}  className={buttonBannerHistory}>Banner History</button>
+    <button onClick={navigateToYourPlanner} className={buttonYourplanner}>Your Planner</button>
     <div className='topbar-user-profile-container'>
         <button className='topbar-navigate-to-login' onClick={navigateToLogin}><img className="topbar-user-profile" src={headerIcon} alt="Wisher" /></button>
     </div>
