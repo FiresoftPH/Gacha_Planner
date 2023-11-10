431dcf45f6262cfadc1722bdcbd37d6025294ab4
Phanuruj3146 <Phanuruj3146@gmail.com>
2023-11-10 16:35:54 +0700

    save button is work now

--- front-end/src/Components/ResultPlanner/resultPlanner.js
+++ front-end/src/Components/ResultPlanner/resultPlanner.js
@@ -12,6 +12,7 @@ function ResultPlanner( props ){
     const [showSuggestion, setShowSuggestion] = useState(false);
     const [showSaveList, setShowSaveList] = useState(false);
 
+    const [currPlanName, setCurrPlanName]= useState("Current plan")
     const inputData = props.userInputData;
     const calData = props.userResultData;
     console.log( inputData );
@@ -26,17 +27,22 @@ function ResultPlanner( props ){
     
     const saveClick = () => {   
         setShowSaveList(!showSaveList);
+        console.log("result planner", showSaveList);
     };
+
+    const changeCurrPlanName = (name) => {
+        setCurrPlanName(name);
+    }
 
     return(
         <div className='gachaPlanner-result-container'>
             <button className='save-button' onClick={saveClick}>
                 <img src={charIcon}></img>
-                <h1>Current Plan</h1>
+                <h1>{currPlanName}</h1>
                 <img className="save-icon" src={saveIcon}></img>
             </button> 
-            <SaveDropdown trigger={showSaveList} inputData={inputData} calData={calData}>
-            </SaveDropdown>
+            {showSaveList && <SaveDropdown trigger={showSaveList} saveClickFunc={saveClick} inputData={inputData} calData={calData} currName={changeCurrPlanName}>
+            </SaveDropdown>}
             <div className='symbol-container'>
                 <div className='info'>
                     <div className="circle-container">
@@ -69,6 +75,8 @@ function ResultPlanner( props ){
                 <li>Currently, you have <span className='input-text'>{inputData.primogems}</span> Primogems and <span className='input-text'>{inputData.crystals}</span> Genesis Crystals. in total you have <span className='result-text'>{calData.currenttotal}</span> Primogems</li>
                 <li>Given that you do the commission, login every day and complete the Battle Pass(if you purchased Gnostic Hymn), you will earn <span className='result-text'>{calData.primosmade}</span> Primogems.</li>
                 <li>In total you will have <span className='result-text'>{calData.primos4free}</span> Primogems in patch <span className='result-text'>{calData.target[0]} { calData.target[1] === 1 ? 'First half' : 'Second half' }</span> which means that you have roll for <span className='result-text'>{calData.fates4free}</span> Intertwined Fates and 60 Primogems.</li>
+                {inputData.fiveorprimos === 0 && <li>For <span className='input-text'>{inputData.fivestars}</span> five star characters, your best case is <span className='input-text'>{calData.bestreq}</span> Primogem and your worse case is <span className='input-text'>{calData.worsereq}</span> Primogems.</li>}
+                {inputData.fiveorprimos === 1 && <li>You need <span className='input-text'>{calData.primoreq}</span> more Primogems to get the amount you want that is <span className='input-text'>{inputData.primowant}</span></li>}
             </ul>
             <button 
                 className={`suggestion-button ${showSuggestion ? 'active' : 'inactive'}`}
@@ -76,7 +84,28 @@ function ResultPlanner( props ){
                 <h1>Suggestion</h1>
                 <img className={`arrow-icon ${showSuggestion ? 'active' : 'inactive'}`} src={arrow}></img>
             </button>
-                {showSuggestion && <div className='informationText'>suggestion</div>}    
+                {showSuggestion && inputData.fiveorprimos === 0 && <div className='informationText'>
+                    <div>5 star best case</div>
+                    <ul>
+                        {calData.bestreq > calData.primos4free && <li>If you purchase <span className='input-text'>{calData.bestwelk}</span> blessing of the Welkin Moon, and <span className='input-text'>{calData.bestbp}</span> Battle pass you can earn more <span className='input-text'>{calData.possible}</span> Primogems</li>}
+                        {calData.bestreq > calData.primos4free && calData.bestextra !== 0 && <li>but still have more <span className='input-text'>{calData.bestextra}</span> Primogems to purchase</li>}
+                        {calData.bestreq <= calData.primos4free && <div>good job, happy gambling</div>}
+                    </ul>
+                    <div>5 star worse case</div>
+                    <ul>
+                        {calData.worsereq > calData.primos4free && <li>If you purchase <span className='input-text'>{calData.worsewelk}</span> blessing of the Welkin Moon, and <span className='input-text'>{calData.worsebp}</span> Battle pass you can earn more <span className='input-text'>{calData.possible}</span> Primogems</li>}
+                        {calData.worsereq > calData.primos4free && calData.worseextra !== 0 && <li>but still have more <span className='input-text'>{calData.worseextra}</span> Primogems to purchase</li>}
+                        {calData.worsereq <= calData.primos4free && <div>good job, happy gambling</div>}
+                    </ul>
+                </div>}    
+                {showSuggestion && inputData.fiveorprimos === 1 && <div className='informationText'>
+                    <div>Primogems as a goal</div>
+                    <ul>
+                        {calData.primoreq > calData.primos4free && <li>If you purchase <span className='input-text'>{calData.planwelk}</span> blessing of the Welkin Moon, and <span className='input-text'>{calData.planbp}</span>  Battle pass you can earn more <span className='input-text'>{calData.possible}</span> Primogems</li>}
+                        {calData.primoreq > calData.primos4free && calData.planextra !== 0 && <li>but still have more <span className='input-text'>{calData.planextra}</span> Primogems to purchase</li>}
+                        {calData.primoreq <= calData.primos4free && <div>good job, happy gambling</div>}
+                    </ul>
+                </div>}
         </div>
     )
 };
--- front-end/src/Components/SaveDropdown/saveDropdown.js
+++ front-end/src/Components/SaveDropdown/saveDropdown.js
@@ -2,66 +2,128 @@ import { useState, useEffect } from 'react';
 import React from 'react';
 import axios from 'axios'
 import './saveDropdown.css';
+import deleteIcon from '../../Pictures/deleteIcon.png'
 
 function SaveDropdown(props) {
-    axios.defaults.baseURL = 'http://localhost:5000'; // Replace with your API URL
+    axios.defaults.baseURL = 'http://localhost:5000/api'; // Replace with your API URL
     axios.defaults.withCredentials = true;
     const numberOfButtons = 5;
     const username = {
-        "username": "furina"    
+        "username": "hutao"    
     }
-    const [savePlannerList, setSavePlannerList] = useState(null);
+    const [savePlannerList, setSavePlannerList] = useState([]);
     const [selectedButton, setSelectedButton] = useState(null);
+    const [saveName, setSaveName] = useState('');
+    const [addSaveName, setAddSaveName] = useState('');
 
-    useEffect(() => {
-        const fetchSaveList = async () => {
-            try {
-                const res = await axios.post('/api/planner/fetch-data', username);
-                console.log('this is save');
-                console.log(res.data);
-            } catch (err) {
-                console.error('Error: ', err);
+    // Move fetchSaveList inside the component as an asynchronous function
+    const fetchSaveList = async () => {
+        try {
+            const res = await axios.post('/user/fetch-data', username);
+
+            if (Array.isArray(res.data)) {
+                setSavePlannerList(res.data);
+                console.log('Data has been fetched:', res.data);
+            } else {
+                console.log('Error: Received non-array data:', res.data);
+                setSavePlannerList([]); // Set an empty array in case of an error
             }
-        };
-        fetchSaveList();
+        } catch (err) {
+            console.error('Error fetching data: ', err);
+            console.log('error here arai');
+            setSavePlannerList([]); // Set an empty array in case of an error
+        }
+    };
+
+    useEffect(() => { fetchSaveList();
     }, [])
 
     const handle_save = (event) => {
         const user_input = props.inputData;
         const program_output = props.calData;
         const load = {
-            "username": "Hu Tao",
+            "username": "hutao",
             "input": user_input,
             "output": program_output,
-            "save_name": "save_name"
+            "save_name": saveName
         }
-        axios.post('/api/planner/save-data', load)
+        axios.post('/planner/save-data', load)
         .then(response => console.log(response))
         .catch(err => console.error('Error: ', err))
+        // setLocalTrigger(!localTrigger);
+        props.saveClickFunc();
+        // console.log('setfalse', localTrigger);
     }
-    const saveClick = (index) => {
-        setSelectedButton(index === selectedButton ? null : index);
-        console.log(index);
+
+    const handle_delete = async (name) => {
+        const del = {
+            "username": "hutao",
+            "save_name": name
+        };
+
+        try {
+            await axios.post('/user/delete-data', del);
+            await fetchSaveList();  // Assuming fetchSaveList is an asynchronous function
+            console.log('Data has been deleted successfully.');
+        } catch (err) {
+            console.error('Error deleting data: ', err);
+        }
+    }
+ 
+    const saveClick = (index, name) => {
+        // setSelectedButton(index === selectedButton ? null : index);
+        setSelectedButton(index);
+        console.log(selectedButton);
+        {name && setSaveName(name)};
     };
 
-    return props.trigger ? (
+    const addingSaveName = (event) => {
+        setAddSaveName(event);
+        setSaveName(event);
+    }
+
+    return (
         <div className='overlay'>
             <div className='saved-list'>
-                {Array.from({ length: numberOfButtons }, (_, index) => (
-                    <button
-                        key={index} 
-                        className={`save-list-button ${index === selectedButton ? 'active' : 'inactive'}`}
-                        onClick={() => saveClick(index)}
-                    >furina
-                    </button>
-                ))}
+                {(savePlannerList.length > 0 || Array.isArray(savePlannerList)) && (savePlannerList.map((item, index) => (
+                    <div className='save-container'>
+                        <button
+                            key={index} 
+                            className={`save-list-button ${index === selectedButton ? 'active' : 'inactive'}`}
+                            onClick={() => saveClick(index, item[0])}
+                        >{item[0]}
+                        </button>
+                        <button className='delete-icon' onClick={() => handle_delete(item[0])}>
+                            <img src={deleteIcon}/>
+                        </button>
+                        
+                    </div>
+                )))}
+                {savePlannerList.length + 1 <= 5 && (
+                    <div className='save-container'>
+                        <button
+                            className={`save-list-button ${savePlannerList.length + 1 === selectedButton ? 'active' : 'inactive'}`}
+                            onClick={() => saveClick(savePlannerList.length + 1, null)}>
+                            {savePlannerList.length + 1 === selectedButton ? (
+                                <input 
+                                    type='text' 
+                                    value={addSaveName}   
+                                    onChange={(e) => addingSaveName(e.target.value)}/>
+                                    ) : (addSaveName !== '' ? addSaveName : '+')}
+                        </button>
+                    </div>)}
+                    {/* {savePlannerList.length + 1 <= 5 && 
+                    <div className='save-container'>
+                        <button>
+                           hi
+                        </button>
+                    </div>} */}
             </div>
             <div className='button-conatiner'>
                 <button className='btn' onClick={handle_save}>Save</button>
-                <button className='btn'>Cancel</button>
             </div>
         </div>
-    ) : '';
+    );
 }
 
 export default SaveDropdown;
